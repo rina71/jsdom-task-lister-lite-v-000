12a97304ec2568eeb80598b6d2088c29a7324a55
Yurina Shin <yurinas@gmail.com>
2020-06-18 04:08:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function prevent(){
   let description = document.getElementById("create-task-form");
   description.addEventListener("submit", function(event){
+alert("me and all my relatives are owned by China");
     event.preventDefault();
   },false)
 }
