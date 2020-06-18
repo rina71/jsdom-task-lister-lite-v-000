f114f38b8f6ad5ddd4b67f41d20cff26d2ccf70c
Yurina Shin <yurinas@gmail.com>
2020-06-18 03:58:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   preventDefault();
 });
+
+function preventDefault(){
+  let description = document.getElementById("new-task-description");
+  description.addEventListener("click", function(event)){
+    event.preventDefault();
+  }
+}
